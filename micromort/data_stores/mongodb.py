7af8516a5e1e57021e25467502bc377647fda70b
Abhinav  Ramesh Kashyap <abhinavkashyap92@gmail.com>
2017-11-08 15:22:31 +0800

    straitstimes headlines scraping

--- micromort/data_stores/mongodb.py
+++ micromort/data_stores/mongodb.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 from micromort.resources.configs.mongodbconfig import mongodb_config
-
+ 
 mongo_client = MongoClient(mongodb_config['host'], mongodb_config['port'])
 mongo_db_singhose = mongo_client[mongodb_config['db']]  # set to your db name
 mongo_collection_articles = mongo_db_singhose[mongodb_config['collection']]  # set to you collection name
--- micromort/resources/configs/mongodbconfig.py
+++ micromort/resources/configs/mongodbconfig.py
@@ -3,5 +3,5 @@
 mongodb_config = {
     'host': 'localhost',
     'port': 27017,
-    'db': 'rss',
-    'collection': 'articles'}
+    'db': 'micromort',
+    'collection': 'straitstimes'}
