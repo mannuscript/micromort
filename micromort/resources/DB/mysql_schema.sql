95c73b6da95bc3364af302550e0c9d36e4b84aa5
Mannu <mannu1200@gmail.com>
2017-12-05 10:45:59 +0800

    Change in the article_social_media_share schema

--- micromort/resources/DB/mysql_schema.sql
+++ micromort/resources/DB/mysql_schema.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.17, for osx10.12 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.20, for Linux (x86_64)
 --
 -- Host: localhost    Database: micromort
 -- ------------------------------------------------------
--- Server version	5.7.17
+-- Server version	5.7.20-0ubuntu0.16.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -25,25 +25,35 @@ DROP TABLE IF EXISTS `article_social_media_shares`;
 CREATE TABLE `article_social_media_shares` (
   `id` int(32) NOT NULL AUTO_INCREMENT,
   `url_id` int(32) DEFAULT NULL,
-  `social_media_channel` varchar(20) DEFAULT NULL,
+  `social_media_channel` varchar(50) DEFAULT NULL,
   `counts` int(32) DEFAULT NULL,
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `FK_url_id` (`url_id`),
   UNIQUE KEY `unique_index` (`url_id`,`social_media_channel`),
+  KEY `FK_url_id` (`url_id`),
   CONSTRAINT `FK_url_id` FOREIGN KEY (`url_id`) REFERENCES `article_urls` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2080 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `article_social_media_shares`
+-- Table structure for table `article_social_media_shares_history`
 --
 
-LOCK TABLES `article_social_media_shares` WRITE;
-/*!40000 ALTER TABLE `article_social_media_shares` DISABLE KEYS */;
-/*!40000 ALTER TABLE `article_social_media_shares` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `article_social_media_shares_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `article_social_media_shares_history` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `url_id` int(32) DEFAULT NULL,
+  `social_media_channel` varchar(50) DEFAULT NULL,
+  `counts` int(32) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `FKH_url_id` (`url_id`),
+  CONSTRAINT `FKH_url_id` FOREIGN KEY (`url_id`) REFERENCES `article_urls` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1523 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `article_urls`
@@ -58,18 +68,8 @@ CREATE TABLE `article_urls` (
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_url` (`url`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3314 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `article_urls`
---
-
-LOCK TABLES `article_urls` WRITE;
-/*!40000 ALTER TABLE `article_urls` DISABLE KEYS */;
-INSERT INTO `article_urls` VALUES (1,'http','2017-09-13 19:00:46');
-/*!40000 ALTER TABLE `article_urls` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -80,4 +80,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-09-13 19:07:36
+-- Dump completed on 2017-12-05 10:41:06
